666f1dd5730d40c365bb54f82e990d979c282bec
PaoloPG2705 <paolomta2705@gmail.com>
2022-07-20 20:49:03 +0200

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@
 import ReactDOM from 'react-dom/client'
 import HeroApp from './HeroApp'
 
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <HashRouter>
+    <BrowserRouter>
     <HeroApp />
-    </HashRouter>
+    </BrowserRouter>
 )
